d730a7f220774c12110e669585523940188adcc0
Raja Ilayaperumal <rajailayaperumal@Rajas-MacBook-Pro.local>
2023-09-19 21:11:13 +0530

    feature: Basic graphQL integration is done.

--- src/components/wrappers/fetch-profile.wrapper.tsx
+++ src/components/wrappers/fetch-profile.wrapper.tsx
@@ -1,7 +1,34 @@
+import { GET_PROFILE_BY_ID_QUERY } from '@/gql/profile.gql';
+import { Profiles } from '@/types/profile.type';
+import { useQuery } from '@apollo/client';
+import { useUserId } from '@nhost/nextjs';
+import { useEffect } from 'react';
+
 interface Props {
   children: React.ReactNode;
 }
 
 export default function FetchProfileComponent({ children }: Props) {
+  // Variables
+  const userId = useUserId();
+
+  // GraphQL
+  const { loading, error, data } = useQuery<Profiles>(GET_PROFILE_BY_ID_QUERY, {
+    variables: {
+      userId
+    }
+  });
+
+  useEffect(() => {
+    if (data) {
+      // TODO: Store in global store
+      console.log(data);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
   return <>{children}</>;
 }
